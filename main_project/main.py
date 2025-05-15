f1c08f6b6cae41c8bed3c0c956f033742c0bfb49
m3linator <milch6060@gmail.com>
2025-05-16 00:15:42 +0200

    Programm zum downloaden fertig implementiert

--- main_project/main.py
+++ main_project/main.py
@@ -1,3 +1,4 @@
+import pytube
 from pytube import YouTube
 
 def downloader():
@@ -5,6 +6,13 @@ def downloader():
 
     url = input("Gebe die URL deines YouTube-Videos ein: \n" )
 
+    youtube = pytube.YouTube(url)
+    video = youtube.streams.get_highest_resolution()            #setzt Streaming-Auflösung
+    #video.download('/Dokumente/Privat/Video-Downloads-PY')      #Download Verzeichnis
+
+    video.download()
+
+    print("Dein Video wurde erfolgreich heruntergeladen!")
 
 def main():
     downloader()
@@ -14,5 +22,5 @@ def main():
 if __name__ == "__main__":
     main()
 
-    
+
 
