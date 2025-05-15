2c3d3b441e826053b901b5716ca8f5fddb2f684c
m3linator <milch6060@gmail.com>
2025-05-15 23:47:49 +0200

    erste Implementierung des Hauptprogramms

---
+++ main_project/main.py
@@ -0,0 +1,18 @@
+from pytube import YouTube
+
+def downloader():
+    """User gibt YT URL ein und kann dann das Video downloaden"""
+
+    url = input("Gebe die URL deines YouTube-Videos ein: \n" )
+
+
+def main():
+    downloader()
+
+
+
+if __name__ == "__main__":
+    main()
+
+    
+
