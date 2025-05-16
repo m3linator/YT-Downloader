b4b330109707ce15735777464533ad9ef5337e00
m3linator <milch6060@gmail.com>
2025-05-16 23:16:25 +0200

    Dateien für die Funktionen erstellt

---
+++ main_project/downloader_alt.py
@@ -0,0 +1,25 @@
+import pytube
+
+
+def downloader():
+    """User gibt YT URL ein und kann dann das Video downloaden"""
+
+    url = input("Gebe die URL deines YouTube-Videos ein: \n" )
+
+    youtube = pytube.YouTube(url)
+    video = youtube.streams.get_highest_resolution()            #setzt Streaming-Auflösung
+    #video.download('/Dokumente/Privat/Video-Downloads-PY')      #Download Verzeichnis
+
+    video.download()
+
+    print("Dein Video wurde erfolgreich heruntergeladen!")
+
+def main():
+    downloader()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
