3705c11387761fbb077bd6bd994e261bffc14ba5
m3linator <milch6060@gmail.com>
2025-05-16 23:51:10 +0200

    Programm zum Ausführen

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import main_project
+from main_project.downloader import vid_download
+
+
+def main():
+
+    link = input("Gib die URL von dem YT-Video an: \n")
+    vid_download(link)
+    print("Dein Video wurde erfolgreich heruntergeladen!")
+
+if __name__ == "__main__":
+    main()
 
